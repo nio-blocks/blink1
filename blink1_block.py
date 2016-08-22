056266cf1c7c5469cf192f913aa82db2fcd92eb7
Kylie Dale <kdale@n.io>
2016-08-22 10:52:08 -0600

    Upgrade block for n.io 2.0

--- blink1_block.py
+++ blink1_block.py
@@ -1,24 +1,24 @@
 from blink1.blink1 import Blink1 as B1
-from nio.metadata.properties.expression import ExpressionProperty
-from nio.metadata.properties.object import ObjectProperty
-from nio.metadata.properties.holder import PropertyHolder
-from nio.common.block.base import Block
-from nio.common.discovery import Discoverable, DiscoverableType
+from nio.properties import Property
+from nio.properties.object import ObjectProperty
+from nio.properties.holder import PropertyHolder
+from nio.block.base import Block
+from nio.util.discovery import discoverable
 
 
 class Color(PropertyHolder):
-    red = ExpressionProperty(title='Red', default='0')
-    green = ExpressionProperty(title='Green', default='0')
-    blue = ExpressionProperty(title='Blue', default='0')
+    red = Property(title='Red', default='0')
+    green = Property(title='Green', default='0')
+    blue = Property(title='Blue', default='0')
 
 
-@Discoverable(DiscoverableType.block)
+@discoverable
 class Blink1(Block):
 
     """ This is the Example block. Put a brief description here. """
 
-    fade_milliseconds = ExpressionProperty(title='Time to fade (ms)',
-                                           default='1000')
+    fade_milliseconds = Property(title='Time to fade (ms)',
+                                 default='1000')
     color = ObjectProperty(Color, title='Color', default=Color())
 
     def __init__(self):
@@ -27,31 +27,31 @@ class Blink1(Block):
 
     def configure(self, context):
         super().configure(context)
-        self._logger.debug('Connecting to Blink1')
+        self.logger.debug('Connecting to Blink1')
         self._blink1 = B1()
-        self._logger.debug('Connected to Blink1')
+        self.logger.debug('Connected to Blink1')
 
     def stop(self):
         try:
             self._blink1.close()
         except:
-            self._logger.exception('Exception while closing Blink1 connection')
+            self.logger.exception('Exception while closing Blink1 connection')
         super().stop()
 
     def process_signals(self, signals, input_id='default'):
         for signal in signals:
             try:
                 fade_milliseconds = int(self.fade_milliseconds(signal))
-                red = int(self.color.red(signal))
-                green = int(self.color.green(signal))
-                blue = int(self.color.blue(signal))
+                red = int(self.color().red(signal))
+                green = int(self.color().green(signal))
+                blue = int(self.color().blue(signal))
             except:
-                self._logger.exception('Failed to evalue fade variables')
+                self.logger.exception('Failed to evalue fade variables')
                 continue
             try:
-                self._logger.debug(
+                self.logger.debug(
                     'Fading to ({}, {}, {}) over {} milliseconds'.format(
                         red, green, blue, fade_milliseconds))
                 self._blink1.fade_to_rgb(fade_milliseconds, red, green, blue)
             except:
-                self._logger.exception('Failed to fade Blink1')
+                self.logger.exception('Failed to fade Blink1')
