e5bd1a80c0f9c5e74f9620819ff57e2fecefef8e
neutralioJenkins <jenkins@n.io>
2017-09-01 16:01:16 -0600

    Bump version to v1.0.0

--- blink1_block.py
+++ blink1_block.py
@@ -18,7 +18,7 @@ class Blink1(TerminatorBlock):
     fade_milliseconds = Property(title='Time to fade (ms)',
                                  default='1000')
     color = ObjectProperty(Color, title='Color', default=Color())
-    version = VersionProperty('1.0.0')
+    version = VersionProperty("1.0.0")
 
     def __init__(self):
         super().__init__()
