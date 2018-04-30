05046399b4228a1fd1fe8b2c9a3eaca27b71bcae
Tyler Lugger <tlugger@n.io>
2018-04-30 13:38:27 -0600

    Add different block for rgb vs string color

--- blink1_rgb_block.py
+++ blink1_rgb_block.py
@@ -1,17 +1,23 @@
 from blink1.blink1 import Blink1 as B1
 
-from nio.properties import (Property, StringProperty, PropertyHolder,
+from nio.properties import (Property, ObjectProperty, PropertyHolder,
                             VersionProperty)
 from nio import TerminatorBlock
 
 
-class Blink1(TerminatorBlock):
+class Color(PropertyHolder):
+    red = Property(title='Red', default='0')
+    green = Property(title='Green', default='0')
+    blue = Property(title='Blue', default='0')
+
+
+class Blink1RGB(TerminatorBlock):
 
     """ Control a blink(1) dongle. """
 
     fade_milliseconds = Property(title='Time to fade (ms)',
                                  default='1000')
-    color = StringProperty(Color, title='Color', default='blue')
+    color = ObjectProperty(Color, title='Color', default=Color())
     version = VersionProperty("1.0.0")
 
     def __init__(self):
@@ -35,14 +41,16 @@ class Blink1(TerminatorBlock):
         for signal in signals:
             try:
                 fade_milliseconds = int(self.fade_milliseconds(signal))
-                color = string(self.color(signal))
+                red = int(self.color().red(signal))
+                green = int(self.color().green(signal))
+                blue = int(self.color().blue(signal))
             except:
                 self.logger.exception('Failed to evalue fade variables')
                 continue
             try:
                 self.logger.debug(
-                    'Fading to {} over {} milliseconds'.format(
-                        color, fade_milliseconds))
-                self._blink1.fade_to_color(fade_milliseconds, color)
+                    'Fading to ({}, {}, {}) over {} milliseconds'.format(
+                        red, green, blue, fade_milliseconds))
+                self._blink1.fade_to_rgb(fade_milliseconds, red, green, blue)
             except:
                 self.logger.exception('Failed to fade Blink1')
